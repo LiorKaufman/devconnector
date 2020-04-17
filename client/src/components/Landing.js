a9b5286b4946353f0e7eb54b996242e1570cabfd
Lior Kaufman <liorka1990@gmail.com>
2020-04-16 18:48:15 -0700

    added theme colors and register form styled

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -19,79 +19,77 @@ const boxContainer = {
 
 export default function Landing() {
   return (
-    <div style={landingImgContainer}>
-      <div style={boxContainer}>
-        <div className='d-flex justify-content-center h-100'>
-          <div className='card landing'>
-            <div className='card-header'>
-              <h3>Sign In</h3>
-              <div className='d-flex justify-content-end social_icon'>
-                <span
-                  style={{
-                    color: '#3b5998',
-                  }}
-                >
-                  <i className='fab fa-facebook-square'></i>
-                </span>
-                <span
-                  style={{
-                    color: '#eeeeee',
-                  }}
-                >
-                  <i className='fab fa-google-plus-square'></i>
-                </span>
-                <span
-                  style={{
-                    color: '#08a0e9',
-                  }}
-                >
-                  <i className='fab fa-twitter-square'></i>
-                </span>
-              </div>
+    <div style={boxContainer}>
+      <div className='d-flex justify-content-center h-100'>
+        <div className='card landing'>
+          <div className='card-header'>
+            <h3>Sign In</h3>
+            <div className='d-flex justify-content-end social_icon'>
+              <span
+                style={{
+                  color: '#3b5998',
+                }}
+              >
+                <i className='fab fa-facebook-square'></i>
+              </span>
+              <span
+                style={{
+                  color: '#eeeeee',
+                }}
+              >
+                <i className='fab fa-google-plus-square'></i>
+              </span>
+              <span
+                style={{
+                  color: '#08a0e9',
+                }}
+              >
+                <i className='fab fa-twitter-square'></i>
+              </span>
             </div>
-            <div className='card-body'>
-              <form>
-                <div className='input-group form-group'>
-                  <div className='input-group-prepend'>
-                    <span className='input-group-text'>
-                      <i className='fas fa-user'></i>
-                    </span>
-                  </div>
-                  <input
-                    type='text'
-                    className='form-control'
-                    placeholder='username'
-                  />
+          </div>
+          <div className='card-body'>
+            <form>
+              <div className='input-group form-group'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text'>
+                    <i className='fas fa-user'></i>
+                  </span>
                 </div>
-                <div className='input-group form-group'>
-                  <div className='input-group-prepend'>
-                    <span className='input-group-text'>
-                      <i className='fas fa-key'></i>
-                    </span>
-                  </div>
-                  <input
-                    type='password'
-                    className='form-control'
-                    placeholder='password'
-                  />
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='username'
+                />
+              </div>
+              <div className='input-group form-group'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text'>
+                    <i className='fas fa-key'></i>
+                  </span>
                 </div>
-                <div className='row align-items-center remember'>
-                  <input type='checkbox' />
-                  Remember Me
-                </div>
-                <Link to='/login' className='btn btn-primary float-right'>
-                  Login
-                </Link>
-              </form>
+                <input
+                  type='password'
+                  className='form-control'
+                  placeholder='password'
+                />
+              </div>
+              <div className='row align-items-center remember'>
+                <input type='checkbox' />
+                Remember Me
+              </div>
+              <Link to='/login' className='btn btn-primary float-right'>
+                Login
+              </Link>
+            </form>
+          </div>
+          <div className='card-footer'>
+            <div className='d-flex justify-content-center links'>
+              Don't have an account?
+              <Link to='/register'>Sign Up</Link>
             </div>
-            <div className='card-footer'>
-              <div className='d-flex justify-content-center links'>
-                Don't have an account?
-                <Link to='/register'>Sign Up</Link>
-              </div>
-              <div className='d-flex justify-content-center'>
-                <a href='/register'>Forgot your password?</a>
-              </div>
+            <div className='d-flex justify-content-center'>
+              <a href='/register'>Forgot your password?</a>
             </div>
           </div>
         </div>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,31 +1,114 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// react router
+import { Link } from 'react-router-dom';
+
+const emptyUser = {
+  name: '',
+  email: '',
+  password: '',
+  passwordCheck: '',
+};
 
 export default function Register() {
+  const [userForm, setUserForm] = useState(emptyUser);
+
+  const { name, email, password, passwordCheck } = userForm;
+
+  const onChange = (e) =>
+    setUserForm({
+      ...userForm,
+      [e.target.name]: e.target.value,
+    });
+
+  const onSumbmit = (e) => {
+    e.preventDefault();
+    if (password !== passwordCheck) {
+      console.log('Passowrds dont match');
+    } else {
+      console.log(userForm);
+    }
+  };
+
   return (
-    <div>
-      Register
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-      <div>111</div>
-    </div>
+    <>
+      <div className='container register'>
+        <div className='registration-body register-card'>
+          <div>
+            <h3 className='register-title register-align'>Register</h3>
+            <p
+              className='register-subtitle register-align'
+              style={{
+                color: '#1fd5b9',
+              }}
+            >
+              it's completely free
+            </p>
+          </div>
+          <form action='' className='register-form' onSubmit={onSumbmit}>
+            <label htmlFor='full-name' className='register-label'>
+              Name
+            </label>
+            <input
+              onChange={(e) => onChange(e)}
+              type='text'
+              id='full-name'
+              className='register-input'
+              name='name'
+              value={name}
+              placeholder='Your full name'
+              required
+            />
+
+            <label htmlFor='email' className='register-label'>
+              Email
+            </label>
+            <input
+              onChange={(e) => onChange(e)}
+              type='text'
+              id='email'
+              value={email}
+              className='register-input'
+              placeholder='Email address'
+              name='email'
+              required
+            />
+
+            <label htmlFor='password' className='register-label'>
+              Password
+            </label>
+            <input
+              onChange={(e) => onChange(e)}
+              type='password'
+              id='password'
+              value={password}
+              name='password'
+              className='register-input'
+              placeholder='Password'
+              required
+            />
+            <label htmlFor='passwordCheck' className='register-label'>
+              Confirm Password
+            </label>
+            <input
+              onChange={(e) => onChange(e)}
+              type='password'
+              id='passwordCheck'
+              className='register-input'
+              name='passwordCheck'
+              value={passwordCheck}
+              placeholder='Confirm Password'
+              required
+            />
+            <button type='submit' className='register-btn'>
+              Create Account
+            </button>
+            <Link to='/login' className='register-login'>
+              I'm already a member
+            </Link>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
