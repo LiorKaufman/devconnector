8aa1a45505c4a0b5e88d89040bcab7b5c548757a
Lior Kaufman <liorka1990@gmail.com>
2020-04-25 19:49:13 -0700

    Prepared for deployment

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
+  //  get the index html path
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
