09b1b9bfdf13b53fbfbf41c86925e95a369af938
Lior Kaufman <liorka1990@gmail.com>
2020-04-26 16:10:59 -0700

    testing 10.6

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 // serve static assets in production
 if (process.env.NODE_ENV === 'production') {
+  console.log('NODE_ENV = production');
   // Set static folder
   app.use(express.static('client/build'));
-
   //  get the index html path
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
