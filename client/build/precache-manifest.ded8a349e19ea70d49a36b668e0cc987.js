2276eeef375b0770aea659ae208bea6fe25c3430
Lior Kaufman <liorka1990@gmail.com>
2020-04-26 09:22:34 -0700

    trying to deploy again

---
+++ client/build/precache-manifest.ded8a349e19ea70d49a36b668e0cc987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e681caa66788b95b89c8a81f45d3c9df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1c5b6e14c41a475003",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a4761dbf08dc1fd80309",
+    "url": "/static/css/main.d89962d0.chunk.css"
+  },
+  {
+    "revision": "5d1c5b6e14c41a475003",
+    "url": "/static/js/2.c4064bc2.chunk.js"
+  },
+  {
+    "revision": "e293c143e177a32373554d6ca4c08bba",
+    "url": "/static/js/2.c4064bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4761dbf08dc1fd80309",
+    "url": "/static/js/main.e32ce174.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
